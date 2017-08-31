f9ac930bbf214e533bca97e908ad101a78907d3a
Thomas Conklin <tconkin63@gmail.com>
2017-08-31 14:58:00 -0700

    Seed DB with random test data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+100.times do
+  User.create(name: Faker::Name.name, email: Faker::Internet.email)
+end
+
+20.times do
+  team = Team.create(name: Faker::Team.creature.titleize, description: Faker::Lorem.sentence)
+  5.times do
+    rand_user = User.all.limit(1).offset(rand(User.count)).first
+    team.users << rand_user if !team.users.include?(rand_user)
+  end
+end
